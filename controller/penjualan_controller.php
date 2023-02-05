e2a589bcb454849c4899455047b300404decb8ea
gideonfrans22 <gideonfrans22@gmail.com>
2023-02-05 21:49:25 +0700

    Update Penjualan controller

--- controller/penjualan_controller.php
+++ controller/penjualan_controller.php
@@ -7,6 +7,43 @@ if(!isset($_SESSION)) {
 require_once "controller/base_controller.php";
 
 class PenjualanController {
+    private $id_penjualan;
+    private $jumlah_penjualan;
+    private $harga_jual;
+    private $id_barang;
+
+    function set_id_penjualan($id_penjualan) {
+        $this->id_penjualan = $id_penjualan;
+    }
+
+    function set_jumlah_penjualan($jumlah_penjualan) {
+        $this->jumlah_penjualan = $jumlah_penjualan;
+    }
+
+    function set_harga_jual($harga_jual) {
+        $this->harga_jual = $harga_jual;
+    }
+
+    function set_id_barang($id_barang) {
+        $this->id_barang = $id_barang;
+    }
+
+    function get_id_penjualan() {
+        return $this->id_penjualan;
+    }
+
+    function get_jumlah_penjualan() {
+        return $this->jumlah_penjualan;
+    }
+
+    function get_harga_jual() {
+        return $this->harga_jual;
+    }
+
+    function get_id_barang() {
+        return $this->id_barang;
+    }
+
     function callasset(){
         $baseController = new BaseController();
         $baseController->callasset();
