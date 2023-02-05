8a67f5f387d37ab25af30776624e9f4293f50abf
gideonfrans22 <gideonfrans22@gmail.com>
2023-02-05 21:34:26 +0700

    Update Hak Akses controller

--- controller/hak_akses_controller.php
+++ controller/hak_akses_controller.php
@@ -7,6 +7,35 @@ if(!isset($_SESSION)) {
 require_once "controller/base_controller.php";
 
 class HakAksesController {
+    private $id_akses;
+    private $nama_akses;
+    private $keterangan;
+
+    function set_id_akses($id_akses) {
+        $this->id_akses = $id_akses;
+    }
+
+    function set_nama_akses($nama_akses) {
+        $this->nama_akses = $nama_akses;
+    }
+
+    function set_keterangan($keterangan) {
+        $this->keterangan = $keterangan;
+    }
+
+    function get_id_akses() {
+        return $this->id_akses;
+    }
+
+    function get_nama_akses() {
+        return $this->nama_akses;
+    }
+
+    function get_keterangan() {
+        return $this->keterangan;
+    }
+
+
     function callasset(){
         $baseController = new BaseController();
         $baseController->callasset();
